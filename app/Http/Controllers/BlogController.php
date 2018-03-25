405e2c003b14c5b4953712bb17c0fa8c599f201f
kalinin1007 <kalinin1007@gmail.com>
2018-03-25 08:23:52 +0300

    migration_and_blade

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class BlogController extends Controller
 {
     public function index()
     {
         $href = route('showPost', ['slug'=>'opp-ruth', 'id'=>258]);
-	return view('index', ['href'=>$href]);
+        $data = DB::select('select * from one');
+	return view('index', ['href'=>$href, 'data'=>$data]);
     }
 	
     public function showPost($slug, $id)
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,11 @@
 <?php
     //dump($href);
+    dump($data);
 ?>
 
 @extends('layouts.base')
 
 @section('footer')
     <h3>new footer</h3>
+    {{ $data[0]->title}}
 @endsection
